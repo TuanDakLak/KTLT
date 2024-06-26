17781a4bf37432965b6a20aaeddd3a22c90c9e93
TuanDakLak <quangtuanxml@gmail.com>
2024-06-26 22:13:19 +0700

    combine menu

--- Project/AcademicStaff.cpp
+++ Project/AcademicStaff.cpp
@@ -375,6 +375,8 @@
         cout << "3. Add new 1st year students to 1st year classes \n";
         cout << "4. Import CSV file containing all students in a specific class to the system\n";
         cout << "5. See list student !\n";
+	cout << "19. Export list student into a csv file \n";
+	cout << "20. Import the scoreboard of a course \n";
         cout << "0. Exit !\n";
         cout << "--------------------------------------------------------------------------------\n";
         int lc;
@@ -405,6 +407,14 @@
             {
                 print(listclasses);
             }
+	      else if (chon == 19)
+ 	    {
+    		ExportCourseInforamtion(l_Course, listclasses);
+ 	    }
+	     else if (chon == 20)
+ 	    {
+     		importfile(l_Course, listclasses);
+ 	    }
             else if (lc == 0)
             {
                 break;
