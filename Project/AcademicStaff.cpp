422dc106fdf1b1eabc06b493066ee8785efe7f9e
TuanDakLak <quangtuanxml@gmail.com>
2024-06-27 16:13:52 +0700

    Fixed bug

--- Project/AcademicStaff.cpp
+++ Project/AcademicStaff.cpp
@@ -375,8 +375,6 @@
         cout << "3. Add new 1st year students to 1st year classes \n";
         cout << "4. Import CSV file containing all students in a specific class to the system\n";
         cout << "5. See list student !\n";
-	cout << "19. Export list student into a csv file \n";
-	cout << "20. Import the scoreboard of a course \n";
         cout << "0. Exit !\n";
         cout << "--------------------------------------------------------------------------------\n";
         int lc;
@@ -407,14 +405,6 @@
             {
                 print(listclasses);
             }
-	      else if (chon == 19)
- 	    {
-    		ExportCourseInforamtion(l_Course, listclasses);
- 	    }
-	     else if (chon == 20)
- 	    {
-     		importfile(l_Course, listclasses);
- 	    }
             else if (lc == 0)
             {
                 break;
@@ -961,6 +951,8 @@ void ViewTheScoreboardOfACourse(ListCourses listcourses, ListClasses listclasses
         cout << "9.View list of course\n";
         cout << "10.Update coures infomartion\n";
         cout << "11.Add a student to the course\n";
+	cout << "19. Export list student into a csv file \n";
+	cout << "20. Import the scoreboard of a course \n";
         cout << "21. View the scoreboard of a course.\n";
         cout << "0.end\n";
         cout << "-----------------------------------------------------\n";
@@ -986,6 +978,14 @@ void ViewTheScoreboardOfACourse(ListCourses listcourses, ListClasses listclasses
                 addStudentToCourse(sinhVien, HocKi.list.head);
 		AddStudent1(studentList, sinhVien);
             }
+	    else if (chon == 19)
+ 	    {
+     		ExportCourseInforamtion(l_Course, listclasses);
+ 	    }
+ 	    else if (chon == 20)
+  	    {
+     		importfile(l_Course, listclasses);
+ 	    }
 	    else if (chon == 21) {
                  ViewTheScoreboardOfACourse(l_Course, listclasses);
             }
